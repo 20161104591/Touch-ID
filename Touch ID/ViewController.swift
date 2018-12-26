cc759b578fbd971844743c31d4d42ba76edb6d5f
20161104591 <346680424@qq.com>
2018-12-26 10:54:38 +0800

    Initial Commit

---
+++ Touch ID/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Touch ID
+//
+//  Created by XYF on 2018/12/26.
+//  Copyright © 2018 XYF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
